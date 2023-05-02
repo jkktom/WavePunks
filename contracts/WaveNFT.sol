21a37361c6ed8d85f24317c012e1ed6072fae110
JK <jkktom@gmail.com>
2023-05-02 14:51:02 +0900

    Mint Done

--- contracts/WaveNFT.sol
+++ contracts/WaveNFT.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WaveNFT is ERC721Enumerable, Ownable {
 
+contract WaveNFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
@@ -32,6 +33,47 @@ contract WaveNFT is ERC721Enumerable, Ownable {
         baseURI = _baseURI;
     }
 
+    function mint(uint256 _mintAmount) public payable {
+        // Only allow minting after specified time
+        // Must mint at least 1 token
+        // Require enough payment
+        // Do not let them mint more tokens than available
+        require(block.timestamp >= allowMintingOn);
+        require(_mintAmount > 0);
+        require(msg.value >= cost * _mintAmount);
+        uint256 supply = totalSupply();
+        require(supply + _mintAmount <= maxSupply);
+
+        // Create tokens
+        for(uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+
+        // Emit event
+        emit Mint(_mintAmount, msg.sender);
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns(string memory)
+    {
+        require(_exists(_tokenId), 'token does not exist');
+        return(string(abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)));
+    }
     
     // Your custom minting function and other logic here
 }
+
+
+
+
+
+
+
+
+
+
+
--- test/WaveNFT.js
+++ test/WaveNFT.js
@@ -65,6 +65,93 @@ describe('WaveNFT', () => {
 
   })
 
+  describe('Minting', () => {
+    let transaction, result
+
+    describe('Success', async () => {
+
+      const ALLOW_MINTING_ON = Date.now().toString().slice(0, 10) // Now
+
+      beforeEach(async () => {
+        const NFT = await ethers.getContractFactory('WaveNFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, false, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+
+        transaction = await nft.connect(minter).mint(1, { value: COST })
+        result = await transaction.wait()
+      })
+
+      it('returns the address of the minter', async () => {
+        expect(await nft.ownerOf(1)).to.equal(minter.address)
+      })
+
+      it('returns total number of tokens the minter owns', async () => {
+        expect(await nft.balanceOf(minter.address)).to.equal(1)
+      })
+
+      it('returns IPFS URI', async () => {
+        // EG: 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/1.json'
+        // Uncomment this line to see example
+        console.log(await nft.tokenURI(1))
+        expect(await nft.tokenURI(1)).to.equal(`${BASE_URI}1.json`)
+      })
+
+      it('updates the total supply', async () => {
+        expect(await nft.totalSupply()).to.equal(1)
+      })
+
+      it('updates the contract ether balance', async () => {
+        expect(await ethers.provider.getBalance(nft.address)).to.equal(COST)
+      })
+
+      it('emits Mint event', async () => {
+        await expect(transaction).to.emit(nft, 'Mint')
+          .withArgs(1, minter.address)
+      })
+
+    })
+
+    describe('Failure', async () => {
+      beforeEach(async () => {
+        const ALLOW_MINTING_ON = Date.now().toString().slice(0, 10) // Now
+        const NFT = await ethers.getContractFactory('WaveNFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, false, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+
+        transaction = await nft.connect(minter).mint(1, { value: COST })
+        result = await transaction.wait()
+      })
+
+      it('rejects insufficient payment', async () => {
+        await expect(nft.connect(minter).mint(1, { value: ether(1) })).to.be.reverted
+      })
+
+      it('requires at least 1 NFT to be minted', async () => {
+        await expect(nft.connect(minter).mint(0, { value: COST })).to.be.reverted
+      })
+
+      it('does not allow more NFTs to be minted than max amount', async () => {
+        await expect(nft.connect(minter).mint(100, { value: COST })).to.be.reverted
+      })
+
+      it('does not return URIs for invalid tokens', async () => {
+        nft.connect(minter).mint(1, { value: COST })
+        await expect(nft.tokenURI('99')).to.be.reverted
+      })
+    })
+
+    describe('Failure', async () => {
+
+      it('rejects minting before allowed time', async () => {
+        const ALLOW_MINTING_ON = new Date('May 26, 2030 18:00:00').getTime().toString().slice(0, 10)
+        const NFT = await ethers.getContractFactory('WaveNFT')
+        nft = await NFT.deploy(NAME, SYMBOL, COST, false, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+
+        await expect(nft.connect(minter).mint(1, { value: COST })).to.be.reverted
+      })
+
+    })
+
+  })
+
 
 })
 
