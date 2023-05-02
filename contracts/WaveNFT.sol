2a831e3c6e6517f307cb81ef4b95260bd4617c9b
JK <jkktom@gmail.com>
2023-05-02 12:51:46 +0900

    Constructor done

--- contracts/WaveNFT.sol
+++ contracts/WaveNFT.sol
@@ -2,19 +2,34 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WaveNFT is ERC721Enumerable {
+contract WaveNFT is ERC721Enumerable, Ownable {
+
     uint256 public cost;
+    uint256 public maxSupply;
+    uint256 public allowMintingOn;
     bool public isBorrowed;
+    string public baseURI;
+    string public baseExtension = ".json";
+
+    event Mint(uint256 amount, address minter);
+    event Withdraw(uint256 amount, address owner);
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _cost,
-        bool _isBorrowed
+        bool _isBorrowed,
+        uint256 _maxSupply,
+        uint256 _allowMintingOn,
+        string memory _baseURI
     ) ERC721(_name, _symbol) {
         cost = _cost;
         isBorrowed = _isBorrowed;
+        maxSupply = _maxSupply;
+        allowMintingOn = _allowMintingOn;
+        baseURI = _baseURI;
     }
 
     
--- test/WaveNFT.js
+++ test/WaveNFT.js
@@ -11,19 +11,30 @@ describe('WaveNFT', () => {
   const NAME = 'WaveNFT'
   const SYMBOL = 'WNFT'
   const COST = ether(10)
-  let nft
+  const MAX_SUPPLY = 15
+  const BASE_URI = 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
+  const ALLOW_MINTING_ON = '1682994263'
+  let nft,
+      deployer,
+      minter
 
-
+  beforeEach(async () => {
+    let accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    minter = accounts[1]
+  })
 
   describe('Deployment', () => {
-
     beforeEach(async () => {
       const WaveNFT = await ethers.getContractFactory('WaveNFT')
       nft = await WaveNFT.deploy(
         'WaveNFT', 
         'WNFT', 
         ether(10),
-        false
+        false,
+        15,
+        1682994263,
+        'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
       )
     })
       it('has correct name', async () => {
@@ -39,8 +50,30 @@ describe('WaveNFT', () => {
       it('is not borrowed', async () => {
         expect(await nft.isBorrowed()).to.equal(false)
       })
+      it('total 15', async () => {
+        expect(await nft.maxSupply()).to.equal(MAX_SUPPLY)
+      })
+      it('minting start time', async () => {
+        expect(await nft.allowMintingOn()).to.equal(ALLOW_MINTING_ON)
+      })
+      it('returns the base URI', async () => {
+        expect(await nft.baseURI()).to.equal(BASE_URI)
+      })
+      it('returns the owner', async () => {
+        expect(await nft.owner()).to.equal(deployer.address)
+      })
 
   })
 
 
 })
+
+
+
+
+
+
+
+
+
+
