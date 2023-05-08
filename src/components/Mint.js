caf3a869239eecfc2c33ce7622a1435ef3fae228
JK <jkktom@gmail.com>
2023-05-08 17:06:00 +0900

    React front end done

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -4,37 +4,42 @@ import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 
 const Mint = ({ provider, nft, cost, setIsLoading }) => {
-  const [isWaiting, setIsWaiting] = useState(false)
+  const [isWaiting, setIsWaiting] = useState(false);
 
   const mintHandler = async (e) => {
-    e.preventDefault()
-    setIsWaiting(true)
+    e.preventDefault();
+    setIsWaiting(true);
 
     try {
-      const signer = await provider.getSigner()
-      const transaction = await nft.connect(signer).mint(1, { value: cost })
-      await transaction.wait()
+      const signer = await provider.getSigner();
+      const transaction = await nft.connect(signer).mint({ value: cost });
+      await transaction.wait();
     } catch {
-      window.alert('User rejected or transaction reverted')
+      window.alert("User rejected or transaction reverted");
     }
 
-    setIsLoading(true)
-  }
+    setIsLoading(true);
+  };
 
-  return(
-    <Form onSubmit={mintHandler} style={{ maxWidth: '450px', margin: '50px auto' }}>
+  return (
+    <Form
+      onSubmit={mintHandler}
+      style={{ maxWidth: "450px", margin: "50px auto" }}
+    >
       {isWaiting ? (
-        <Spinner animation="border" style={{ display: 'block', margin: '0 auto' }} />
+        <Spinner
+          animation="border"
+          style={{ display: "block", margin: "0 auto" }}
+        />
       ) : (
         <Form.Group>
-          <Button variant="primary" type="submit" style={{ width: '100%' }}>
+          <Button variant="primary" type="submit" style={{ width: "100%" }}>
             Mint
           </Button>
         </Form.Group>
       )}
-
     </Form>
-  )
-}
+  );
+};
 
 export default Mint;
