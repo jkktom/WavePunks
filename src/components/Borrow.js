0c3df17a06cf499afa40dca25f8493ee329606b0
JK <jkktom@gmail.com>
2023-05-22 00:06:33 +0900

    done - borrow page renovation. WIP status page

--- src/components/Borrow.js
+++ src/components/Borrow.js
@@ -7,18 +7,19 @@ import { ethers } from 'ethers'
 
 import Alert from 'react-bootstrap/Alert';
 
-
 import Loading from './Loading';
 
 import {
   loadAllOffers,
   cancelLendingOffer,
   borrowToken,
+  tokenCurrentStatus,
   redeemToken
 } from '../store/interactions'
 
 const Borrow = () => {
   const [tokenId, setTokenId] = useState('');
+	const [tokenStates, setTokenStates] = useState({});
 	const provider = useSelector(state => state.provider.connection)
 	const account = useSelector(state => state.provider.account);
 
@@ -30,6 +31,7 @@ const Borrow = () => {
     try {
       await cancelLendingOffer(provider, nft, tokenId, dispatch);
       alert('Lending offer cancelled successfully');
+	    window.location.reload();
     } catch (error) {
       console.error('Error cancelling lending offer:', error);
       alert('Error cancelling lending offer');
@@ -41,22 +43,24 @@ const Borrow = () => {
     	const offer = offers.find((offer) => offer.args.tokenId.toString() === tokenId);
       await borrowToken(provider, nft, tokenId, offer.args.deposit, dispatch);
       alert('Successfully Borrowed NFT ');
+	    window.location.reload();
     } catch (error) {
       console.error('Error Borrwing:', error);
       alert('Error Borrowing');
     }
   };
 
-  const redeemHandler = async (tokenId) => {
-    try {
-    	const offer = offers.find((offer) => offer.args.tokenId.toString() === tokenId);
-      await redeemToken(provider, nft, tokenId, offer.args.deposit, dispatch);
-      alert('Successfully Redeemed NFT ');
-    } catch (error) {
-      console.error('Error Redeemed:', error);
-      alert('Error Redeemed');
-    }
-  };
+	useEffect(() => {
+	  const loadTokenStates = async () => {
+	    const newTokenStates = {};
+	    for (const offer of offers) {
+	      newTokenStates[offer.args.tokenId.toString()] = await tokenCurrentStatus(provider, nft, offer.args.tokenId.toString(), dispatch);
+	    }
+	    setTokenStates(newTokenStates);
+	  };
+
+	  loadTokenStates();
+	}, [nft, offers, provider, dispatch]);
 
   useEffect(() => {
     if (provider && nft) {
@@ -86,48 +90,52 @@ const Borrow = () => {
 						</tr>
           </thead>
           <tbody>
-            {offers && offers.map((offer, index) => (
-              <tr key={index}>
-                <td style={css}>{offer.hash.slice(0, 5) + '...' + offer.hash.slice(61, 66)}</td>
-						    <td style={css}>{offer.args.tokenId.toString()}</td>
-						    <td style={css}>{ethers.utils.formatUnits(offer.args.deposit.toString(), 'ether')}</td>
-						    <td style={css}>{new Date(Number(offer.args.lendingStartTime.toString() + '000')).toLocaleDateString(
-						        undefined,
-						        {
-						          year: 'numeric',
-						          month: 'long',
-						          day: 'numeric',
-						          hour: '2-digit',
-						          minute: '2-digit',
-						          hour12: false
-						        }
-						      )}
-						    </td>
-						    <td style={css}>{new Date(Number(offer.args.lendingExpiration.toString() + '000')).toLocaleDateString(
-							        undefined,
-							        {
-							          year: 'numeric',
-							          month: 'long',
-							          day: 'numeric',
-							          hour: '2-digit',
-							          minute: '2-digit',
-							          hour12: false
-							        }
-							      )}
-							  </td>
-						    <td style={css}>
-								  {Math.floor(offer.args.redemptionPeriod.toNumber() / 60)} minutes
-								  {' '}   
-								  {offer.args.redemptionPeriod.toNumber() % 60} seconds
-								</td>
-						    <td style={css}>
-						    	<Button onClick={() => borrowHandler(offer.args.tokenId.toString())}>Borrow</Button>
-								</td>
-						    <td style={css}>
-						    	<Button onClick={() => cancelOffer(offer.args.tokenId.toString())}>Cancel</Button>
-								</td>
-              </tr>
-            ))}
+            {offers && offers.map((offer, index) => {
+					    if(tokenStates[offer.args.tokenId.toString()] === 'Lending is Open') {
+					    	return (
+		              <tr key={index}>
+		                <td style={css}>{offer.hash.slice(0, 5) + '...' + offer.hash.slice(61, 66)}</td>
+								    <td style={css}>{offer.args.tokenId.toString()}</td>
+								    <td style={css}>{ethers.utils.formatUnits(offer.args.deposit.toString(), 'ether')}</td>
+								    <td style={css}>{new Date(Number(offer.args.lendingStartTime.toString() + '000')).toLocaleDateString(
+								        undefined,
+								        {
+								          year: 'numeric',
+								          month: 'long',
+								          day: 'numeric',
+								          hour: '2-digit',
+								          minute: '2-digit',
+								          hour12: false
+								        }
+								      )}
+								    </td>
+								    <td style={css}>{new Date(Number(offer.args.lendingExpiration.toString() + '000')).toLocaleDateString(
+									        undefined,
+									        {
+									          year: 'numeric',
+									          month: 'long',
+									          day: 'numeric',
+									          hour: '2-digit',
+									          minute: '2-digit',
+									          hour12: false
+									        }
+									      )}
+									  </td>
+								    <td style={css}>
+										  {Math.floor(offer.args.redemptionPeriod.toNumber() / 60)} minutes
+										  {' '}   
+										  {offer.args.redemptionPeriod.toNumber() % 60} seconds
+										</td>
+								    <td style={css}>
+								    	<Button onClick={() => borrowHandler(offer.args.tokenId.toString())}>Borrow</Button>
+										</td>
+								    <td style={css}>
+								    	<Button onClick={() => cancelOffer(offer.args.tokenId.toString())}>Cancel</Button>
+										</td>
+		              </tr>
+					    	)
+					    }
+            })}
           </tbody>
         </Table>
       ) : (
